18c3a351b472adfb645fa2740e45a66f93001c09
james Olaya <jamesmixolaya@gmail.com>
2024-05-10 12:01:10 -0500

    creation search component

--- docs/blocks/Components/schemes/schemeQblock.jsx
+++ docs/blocks/Components/schemes/schemeQblock.jsx
@@ -1202,8 +1202,6 @@ export const Element = {
   ]
 }
 
-
-
 export const sliderOWL = {
   "tabs": [
     {
@@ -1304,5 +1302,52 @@ export const sliderOWL = {
       }
     },
     
+  ]
+}
+
+export const contentSearch = {
+  "tabs": [
+    {
+      "title": "General",
+        "table": {
+          "data":[
+            [
+              'Search Title(en)',
+              {type: <code>Search Title(en) → input</code>, description: <>Used to add a title that the search engine will have in the block</>},
+            ],
+            [
+              'Search engine placeholder (en)',
+              {type: <code>Search engine placeholder → input</code>, description: <>Used to add text in the search engine so that the user can view it before entering text in the block search engine</>},
+            ],
+          ]
+        }
+    },
+  ]
+}
+export const search = {
+  "tabs": [
+    {
+      "title": "General",
+      "table": {
+        "data": [
+          [
+            'Layout ',
+            { type: <code>Layout → select (layout types)</code>, description: <>Used to define the type of layout that is going to be used in the block</>},
+          ],
+          [
+            'Icon',
+            { type: <code>Icon → input (awesome font)</code>, description: <>Used to add an awesome font icon to the block search engine</>},
+          ],
+          [
+            'Class button',
+            { type: <code>Class button → input(HTML)</code>, description: <>Used to add classes to the block's search button</>},
+          ],
+          [
+            'Button styles',
+            { type: <code>Button styles → input(CSS)</code>, description: <>Used to add styles with CSS naming of the search button</>},
+          ],
+        ]
+      }
+    },
   ]
 }
---
+++ docs/blocks/Components/search.md
@@ -0,0 +1,12 @@
+---
+title: "Search"
+---
+
+import {TableTabs} from '@site/src/components/table/table.jsx';
+import {contentSearch, search} from './schemes/schemeQblock.jsx';
+
+## content
+<TableTabs tabsContent={contentSearch} />
+
+## attribute
+<TableTabs tabsContent={search} />
