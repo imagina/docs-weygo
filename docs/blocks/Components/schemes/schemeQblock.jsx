b29f5fc7ed73e5293eb0a7287a07d322f3636b5e
james Olaya <jamesmixolaya@gmail.com>
2024-05-10 14:04:17 -0500

    creation newsletter component

--- docs/blocks/Components/schemes/schemeQblock.jsx
+++ docs/blocks/Components/schemes/schemeQblock.jsx
@@ -1202,8 +1202,6 @@ export const Element = {
   ]
 }
 
-
-
 export const sliderOWL = {
   "tabs": [
     {
@@ -1304,5 +1302,68 @@ export const sliderOWL = {
       }
     },
     
+  ]
+}
+
+export const contentNews = {
+  "tabs": [
+    {
+      "title": "General",
+        "table": {
+          "data":[
+            [
+              'Title (en)',
+              {type: <code>Title → input</code>, description: <>Used to add a title at the top of the Newsletter block</>},
+            ],
+            [
+              'Description (en)',
+              {type: <code>Description → input</code>, description: <>Used to add a description at the top of the Newsletter block</>},
+            ],
+            [
+              'Post description (en)',
+              {type: <code>Post description → input</code>, description: <>Used to add text after the description at the top of the Newsletter block</>},
+            ],
+            [
+              'Button label (en)',
+              {type: <code>Button label → input</code>, description: <>Used to add text to a button at the top of the Newsletter block</>},
+            ],
+          ]
+        }
+    },
+  ]
+}
+export const news = {
+  "tabs": [
+    {
+      "title": "General",
+      "table": {
+        "data": [
+          [
+            'Newsletter Template',
+            { type: <code>Newsletter Template → select</code>, description: <>Used to define the template that will be used in the Newsletter block</>},
+          ],
+          [
+            'Classes for title',
+            { type: <code>Classes for title → input</code>, description: <>Used to add classes to the Newsletter block title</>},
+          ],
+          [
+            'Classes for description',
+            { type: <code>Classes for description → input</code>, description: <>Used to add classes to the Newsletter block description</>},
+          ],
+          [
+            'Classes for post description',
+            { type: <code>Classes for post description → input</code>, description: <>Used to add classes to the post description of the Newsletter block</>},
+          ],
+          [
+            'Classes for input',
+            { type: <code>Classes for input → input</code>, description: <>Used to add classes to the input of the Newsletter block</>},
+          ],
+          [
+            'Classes for the button',
+            { type: <code>Classes for the button → input</code>, description: <>Used to add classes to the Newsletter block button</>},
+          ],
+        ]
+      }
+    },
   ]
 }
---
+++ docs/blocks/newsletter.md
@@ -0,0 +1,12 @@
+---
+title: "Newsletter"
+---
+
+import {TableTabs} from '@site/src/components/table/table.jsx';
+import {contentNews} from './schemes/schemeQblock.jsx';
+
+## content
+<TableTabs tabsContent={contentNews} />
+
+## attribute
+<TableTabs tabsContent={} />
